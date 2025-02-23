e7e07c945c4ab1c1b7a872c78c0a7c9503911983
Enmanuel Maria Iglesias Garcia Del Castillo <eiglesia@c2r2s3.42madrid.com>
2025-02-23 20:33:13 +0100

    versionfinal

--- ex00/rush02.c
+++ ex00/rush02.c
@@ -12,6 +12,29 @@
 
 void	ft_putchar(char c);
 
+static void	ft_putcoord(int i, int j, int x, int y)
+{
+	if (j == 0 || j == y)
+	{
+		if (i == 0 || i == x)
+		{
+			if (j == 0)
+				ft_putchar('A');
+			else
+				ft_putchar('C');
+		}
+		else
+			ft_putchar('B');
+	}
+	else
+	{
+		if (i == 0 || i == x)
+			ft_putchar('B');
+		else
+			ft_putchar(' ');
+	}
+}
+
 void	rush(int x, int y)
 {
 	int	i;
@@ -25,25 +48,7 @@ void	rush(int x, int y)
 		i = 0;
 		while (i <= x)
 		{
-			if (j == 0 || j == y)
-			{
-				if (i == 0 || i == x)
-				{
-					if (j == 0)
-						ft_putchar('A');
-					else
-						ft_putchar('C');
-				}
-				else
-					ft_putchar('B');
-			}
-			else
-			{
-				if (i == 0 || i == x)
-					ft_putchar('B');
-				else
-					ft_putchar(' ');
-			}
+			ft_putcoord(i, j, x, y);
 			i++;
 		}
 		ft_putchar('\n');
